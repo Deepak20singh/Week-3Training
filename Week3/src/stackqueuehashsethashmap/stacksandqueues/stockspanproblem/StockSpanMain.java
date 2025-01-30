89426d8e5ac46ed5f1ee33658f6d7cc4829fd459
Deepak <singh017deepak@gmail.com>
2025-01-30 16:31:50 +0530

    Stock Span Problem

---
+++ Week3/src/stackqueuehashsethashmap/stacksandqueues/stockspanproblem/StockSpanMain.java
@@ -0,0 +1,10 @@
+package stackqueuehashsethashmap.stacksandqueues.stockspanproblem;
+
+public class StockSpanMain {
+    public static void main(String[] args) {
+        StockSpan s=new StockSpan();
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};  // Example stock prices
+        s.calculate(prices);
+        s.display();
+    }
+}
