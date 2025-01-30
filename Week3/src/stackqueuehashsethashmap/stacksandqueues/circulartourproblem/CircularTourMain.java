feb9d135a07df67aea5c75aea5e8fe99e061328b
Deepak <singh017deepak@gmail.com>
2025-01-30 16:28:47 +0530

    Circular Tour Problem

---
+++ Week3/src/stackqueuehashsethashmap/stacksandqueues/circulartourproblem/CircularTourMain.java
@@ -0,0 +1,21 @@
+package stackqueuehashsethashmap.stacksandqueues.circulartourproblem;
+
+public class CircularTourMain {
+    public static void main(String[] args) {
+        CircularTour c=new CircularTour();
+        int[][] pumps = {
+                {4, 6},
+                {6, 5},
+                {7, 3},
+                {4, 5}
+        };
+
+        int result = c.result(pumps);
+        if (result == -1) {
+            System.out.println("Circular tour is not possible.");
+        } else {
+            System.out.println("Start at Petrol Pump: " + result);
+        }
+
+    }
+}
