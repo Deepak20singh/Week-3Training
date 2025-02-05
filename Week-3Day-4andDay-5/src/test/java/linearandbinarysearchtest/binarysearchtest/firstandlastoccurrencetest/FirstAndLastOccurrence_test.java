25d16fd5732b41f895d2a9c00311019512fea71a
Deepak <singh017deepak@gmail.com>
2025-02-05 10:57:40 +0530

    [Deepak] Add .First occurance code with its test file

---
+++ Week-3Day-4andDay-5/src/test/java/linearandbinarysearchtest/binarysearchtest/firstandlastoccurrencetest/FirstAndLastOccurrence_test.java
@@ -0,0 +1,25 @@
+package linearandbinarysearchtest.binarysearchtest.firstandlastoccurrencetest;
+import linearandbinarysearch.binarysearch.firstandlastoccurrence.FirstAndLastOccurrance;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class FirstAndLastOccurrence_test {
+
+    @Test
+    public void tester(){
+        FirstAndLastOccurrance firstlast=new FirstAndLastOccurrance();
+        int[] arr={1,1,1,1,3,3,3,4,5,5,6,7,8,9};
+        int target=1;
+        int index=firstlast.binarySearch(arr,target);
+        int first=firstlast.firstOccurance(arr,index);
+        int last=firstlast.lastOccurance(arr,index);
+
+        Assert.assertEquals(0,first);
+        Assert.assertEquals(3,last);
+
+    }
+
+
+}
