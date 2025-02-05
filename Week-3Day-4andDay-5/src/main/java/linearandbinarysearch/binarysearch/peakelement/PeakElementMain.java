d671f75d35ef708d9f4fd8be07c92d0074f629c6
Deepak <singh017deepak@gmail.com>
2025-02-05 11:00:24 +0530

    [Deepak] Add .Peek Element code with its test file

---
+++ Week-3Day-4andDay-5/src/main/java/linearandbinarysearch/binarysearch/peakelement/PeakElementMain.java
@@ -0,0 +1,13 @@
+package linearandbinarysearch.binarysearch.peakelement;
+
+import java.util.ArrayList;
+
+import static linearandbinarysearch.binarysearch.peakelement.PeakElement.search;
+
+public class PeakElementMain {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,45,6,8,5,9,0};
+        ArrayList result=search(arr);
+        System.out.println(result.toString());
+    }
+}
