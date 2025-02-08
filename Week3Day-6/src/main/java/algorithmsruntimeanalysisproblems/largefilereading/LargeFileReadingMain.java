e1a44e623c5dd7ab1b61e53e89d1e0fbcb6fb288
Deepak <singh017deepak@gmail.com>
2025-02-08 19:18:14 +0530

    [Deepak] Add. Code of comparing reading time of files with its test file

---
+++ Week3Day-6/src/main/java/algorithmsruntimeanalysisproblems/largefilereading/LargeFileReadingMain.java
@@ -0,0 +1,19 @@
+package algorithmsruntimeanalysisproblems.largefilereading;
+
+import static algorithmsruntimeanalysisproblems.largefilereading.FileReaderCode.readFile;
+import static algorithmsruntimeanalysisproblems.largefilereading.InputStreamCode.readFileInput;
+
+public class LargeFileReadingMain {
+    public static void main(String[] args) {
+        String filePathName="src/main/java/algorithmsruntimeanalysisproblems/largefilereading/Sample.txt";
+        long startTime1=System.nanoTime();
+        readFile(filePathName);
+        long endTime1=System.nanoTime();
+        long startTime2=System.nanoTime();
+        readFileInput(filePathName);
+        long endTime2=System.nanoTime();
+        System.out.println("File Reading By File Reader :- "+(endTime1-startTime1));
+        System.out.println("File Reading By InputStream Reader :- "+(endTime2-startTime2));
+
+    }
+}
